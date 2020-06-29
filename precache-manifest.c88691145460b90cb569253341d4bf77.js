878b53c8e29b3d8c75580145a459cea4972f8555
Grzegorz Pokorny <grzegorz.pokorny.94@gmail.home>
2020-06-29 05:13:50 +0200

    Updates

---
+++ precache-manifest.c88691145460b90cb569253341d4bf77.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2124d035925b6056ad6762e7a02c2853",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3aced569bd1cb56163f6",
+    "url": "/static/css/main.341a9312.chunk.css"
+  },
+  {
+    "revision": "40799614fec85c15e65c",
+    "url": "/static/js/2.5c562860.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.5c562860.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3aced569bd1cb56163f6",
+    "url": "/static/js/main.30252989.chunk.js"
+  },
+  {
+    "revision": "1b044b4b5dc0a2d0761d",
+    "url": "/static/js/runtime-main.e9c0511b.js"
+  }
+]);
