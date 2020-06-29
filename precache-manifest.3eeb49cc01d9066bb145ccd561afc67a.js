1048dce9937b5f2c9f12370066c68ebc9c4ae6bc
Grzegorz Pokorny <grzegorz.pokorny.94@gmail.home>
2020-06-29 05:20:19 +0200

    Updates

---
+++ precache-manifest.3eeb49cc01d9066bb145ccd561afc67a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7044b730e3c86ba45e6774166e592a1b",
+    "url": "/currency-converter/index.html"
+  },
+  {
+    "revision": "9460183dde8195cb9fb0",
+    "url": "/currency-converter/static/css/main.341a9312.chunk.css"
+  },
+  {
+    "revision": "40799614fec85c15e65c",
+    "url": "/currency-converter/static/js/2.5c562860.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/currency-converter/static/js/2.5c562860.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9460183dde8195cb9fb0",
+    "url": "/currency-converter/static/js/main.aa44e8fc.chunk.js"
+  },
+  {
+    "revision": "4aa4730b9694d75daade",
+    "url": "/currency-converter/static/js/runtime-main.d7dce62c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c88691145460b90cb569253341d4bf77.js"
+  "/currency-converter/precache-manifest.3eeb49cc01d9066bb145ccd561afc67a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/currency-converter/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
